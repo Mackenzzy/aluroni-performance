43dcebddbd685d877264caf706f631b18dc0842e
Mackenzzy <mackenzzy@gmail.com>
2023-11-27 20:11:11 -0300

    Refatorando as rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,22 @@
 import Footer from 'components/Footer';
 import Menu from 'components/Menu';
-import PaginaPadrao from 'components/PaginaPadrao';
 // import Cardapio from 'pages/Cardapio';
-import Inicio from 'pages/Inicio';
-import NotFound from 'pages/NotFound';
-import Prato from 'pages/Prato';
-import Sobre from 'pages/Sobre';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Cardapio = lazy(() => import('pages/Cardapio'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+const Inicio = lazy(() => import('pages/Inicio'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Prato = lazy(() => import('pages/Prato'));
+const Sobre = lazy(() => import('pages/Sobre'));
 
 export default function AppRouter() {
   return (
     <main className='container'>
       <Router>
+      <Menu />
         <Suspense fallback={<p>Carregando...</p>}>
-          <Menu />
           <Routes>
             <Route path='/' element={<PaginaPadrao />}>
               <Route index element={<Inicio />} />
@@ -26,8 +26,8 @@ export default function AppRouter() {
             <Route path='prato/:id' element={<Prato />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
-          <Footer />
         </Suspense>
+        <Footer />
       </Router>
     </main>
   );
